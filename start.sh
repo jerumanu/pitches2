8b34d8ce7e1ccf6f57cc1fa871e3b018f2771a4d
jerumanu <emmanueljeru200@gmail.com>
2020-12-10 05:07:25 +0300

    add start key

---
+++ start.sh
@@ -0,0 +1,7 @@
+export MAIL_USERNAME='emmanueljeru200@gmail.com'
+export MAIL_PASSWORD='915477kk'
+export SECRET_KEY='915477kk'
+SQLALCHEMY_DATABASE_URI= 'postgresql+psycopg2://moringa:915477kk@localhost/pitches'
+
+
+python3.8 manage.py server
